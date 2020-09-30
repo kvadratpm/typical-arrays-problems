f7b79c3d933af7228e11e3a04abf98acde1ab6cc
Maksim Perevyazkin <kvadratpm@mail.ru>
2020-09-30 10:24:25 +0300

    init: start task typical-arrays-problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+    if (array.length === 0 || array === 0) {
+        return 0; 
+    }
+    let min = array[0];
+    array.map((elem) => {
+        if (elem < min) {
+            min = elem;
+        }
+    })
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+    if (array.length === 0 || array === 0) {
+        return 0; 
+    }
+    let max = array[0];
+    array.map((elem) => {
+        if (elem > max) {
+            max = elem;
+        }
+    })
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+    if (array.length === 0 || array === 0) {
+        return 0; 
+    }
+    return array.reduce((acc, elem) => acc += elem, 0) / array.length;
 }
